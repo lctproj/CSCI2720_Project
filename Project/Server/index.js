ca68d4d9f1ccfbd0aa80237284f33f992293039a
iamtlcs <simoncheung2002@gmail.com>
2023-12-05 16:26:46 +0800

    RESTAPI to get all events

--- Project/Server/index.js
+++ Project/Server/index.js
@@ -1,29 +1,48 @@
-
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// Create an Express server
 const app = express();
 app.use(cors());
 
+const uri = "mongodb+srv://CSCi2720:CSCI2720@csci2720project.nrracpp.mongodb.net/?retryWrites=true&w=majority";
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/mydatabase', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-  });
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// Define routes and middleware here
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    const database = client.db('CSCI2720Project'); // Specify the database name
+    return database;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+}
 
-// Start the server
-const port = 3000;
+const port = 8964;
 app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+app.get('/all-events', async (req, res) => {
+  try {
+    const database = await connectToDatabase(); // Connect to the database
+    const collection = database.collection('events'); // Get the collection
+    const items = await collection.find().toArray(); // Fetch all items in the collection
+    res.json(items); // Send the items as a JSON response
+  } catch (error) {
+    console.error('Error retrieving items:', error);
+    res.status(500).json({ error: 'An error occurred while retrieving items' });
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
   console.log(`Server is running on port ${port}`);
 });

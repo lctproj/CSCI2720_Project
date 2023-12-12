ce96d55db1b2631e563152b5ce2233ff210f8970
LCT <136698906+lctproj@users.noreply.github.com>
2023-12-13 01:33:43 +0800

    big changes on eventmain

--- Project/src/assets/OLD_EventMain.jsx
+++ Project/src/assets/OLD_EventMain.jsx
@@ -1,7 +1,9 @@
+/*as backup reference only
+
 import "./eventmain.css";
 import EventFilterBar from "./EventFilterBar.jsx";
 import EventCard from "./EventCard.jsx";
-import React, {useState} from "react";
+import React, {useState,useEffect} from "react";
 import { HiArrowsUpDown, HiOutlineArrowSmallUp , HiOutlineArrowSmallDown  } from "react-icons/hi2";
 
 
@@ -44,20 +46,36 @@ const ArrowSign = ({ category, value, ascending }) => {
 
 
 export default function EventMain (){
-    //search parameters
+    useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch("http:/localhost:8964/all-events", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({})
+            });
+            const data = await response.json();
+            setEvents(data);
+        } catch (err) {
+            return(
+                <p>Error fetching events from database</p>
+            )
+        }
+    };
+
+    fetchData();
+    }, []); //fetch data through the API, from the db
+
     const [searchInput,setInput]=useState('');
     const [maxPrice,setMaxPrice] = useState(750);
     const [earliestDate,setEarliestDate] = useState('');
     const [latestDate,setLatestDate] = useState('');
-
-    //search results
-    const [results,setResults] = useState(null);
-
-    //sorting
     const [category ,setCategory] = useState('');
     const [ascending,setAscending] = useState(true);
+    const [events, setEvents] = useState([]);
 
-    //input-related event handlers
     const handleSearchInput = (value) => {
         setInput(value);
     };
@@ -66,6 +84,60 @@ export default function EventMain (){
         setMaxPrice(value);
     }
 
+    const getEarliestDate = async (event) => {
+        try{
+            const response = await fetch('http://localhost:8964/event-dates',{
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({})
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            
+            const eventDates = await response.json();
+            const eventID = event._id;
+
+            let corrEventinDate = eventDates.find(entry=>entry._id===eventID).indate;
+            return corrEventinDate[0].splice(0,10).toString;
+        }catch(err){
+            return (
+                <p>Error fetching earliest event date</p>
+            )
+        }
+
+    };
+  
+    const getLatestDate = async (event) => {
+        try{
+            const response = await fetch('http://localhost:8964/event-dates',{
+                method:'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({})
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            
+            const eventDates = await response.json();
+            const eventID = event._id;
+
+            let corrEventinDate = eventDates.find(entry=>entry._id===eventID).indate;
+            return corrEventinDate[corrEventinDate.length-1].splice(0,10).toString;
+        }catch(err){
+            return (
+                <p>Error fetching latest event date</p>
+            )
+        }
+
+    };
+
     const handleEarliestDateChange = (value)=>{
         setEarliestDate(value);
     }
@@ -73,18 +145,6 @@ export default function EventMain (){
     const handleLatestDateChange = (value) =>{
         setLatestDate(value);
     }
-
-    const searchParams = {
-        "name" : searchInput,
-        "price" : maxPrice,
-        "earliestDate" : earliestDate,
-        "latestDate" : latestDate
-    }
-
-//change search results
-const handleResults = (results) => {
-    setResults(results);
-  };
 
 //changes the sorting category
     const handleCategory = (value) => {
@@ -96,22 +156,32 @@ const handleResults = (results) => {
         }
     }
 
+// cleans the 'price' property into an array of numbers
+    const processedPrice=(string)=>{
+        return string.replace(/$/g,'').split(',').map(price => Number(price));
+    } 
+
         
-    const finalResult = results.sort((a, b) => { //sorts the array
+    const filteredEvents = events.filter((event) =>
+    event.titlee.toLowerCase().includes(searchInput.toLowerCase()) //filter by title
+    ).filter((event) =>Math.max(...processedPrice(event.pricee))<maxPrice //filter by price
+    ).filter((event) =>getEarliestDate(event)>earliestDate) // filter by earliest date
+    .filter((event) =>getLatestDate(event)<latestDate) //filter by latest date
+    .sort((a, b) => { //sorts the array
         let aValue, bValue;
 
         if (category === 'earliestdate') {
-            aValue = a.earliestDate;
-            bValue = b.earliestDate;
+            aValue = getEarliestDate(a);
+            bValue = getEarliestDate(b);
         } else if (category === 'latestdate') {
-            aValue = a.latestDate;
-            bValue = b.latestDate;
+            aValue = getLatestDate(a);
+            bValue = getLatestDate(b);
         } else if (category === 'price') {
             aValue = Math.min(...a.price); 
             bValue = Math.min(...b.price);  
         } else {
-            aValue = a.name;
-            bValue = b.name;
+            aValue = a.eventname;
+            bValue = b.eventname;
         }
 
         if (typeof aValue === 'string') {
@@ -124,18 +194,16 @@ const handleResults = (results) => {
 
     return(
         <div className="event-main">
-            <EventFilterBar onInputChange={handleSearchInput} onPriceChange={handlePriceChange} 
-                onEarliestDateChange={handleEarliestDateChange} onLatestDateChange={handleLatestDateChange} 
-                searchParams={searchParams} onResult={handleResults}/>
+            <EventFilterBar onInputChange={handleSearchInput} onPriceChange={handlePriceChange} onEarliestDateChange={handleEarliestDateChange} onLatestDateChange={handleLatestDateChange}/>
             <HeaderBar handleCategory={handleCategory} category={category} ascending={ascending} />
-            {finalResult.map((event, index) => (
+            {filteredEvents.map((event, index) => (
                 <EventCard
                 key={index}
                 id={event._id}
                 eventname={event.eventname}
-                earliestdate={event.earliestDate}
-                latestdate={event.latestDate}
-                price={event.price}
+                earliestdate={getEarliestDate(event)}
+                latestdate={getLatestDate(event)}
+                price={event.price.toSorted((a, b) => a - b).toString()}
                 />
             ))}
         </div>
@@ -143,3 +211,5 @@ const handleResults = (results) => {
 
    
 }
+
+*/

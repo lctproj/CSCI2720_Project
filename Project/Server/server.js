5479516b77b87b08c6132438fa8d393b668c4a98
eddyng34 <eddyng34@gmail.com>
2023-12-13 17:32:01 +0800

    localStorage implementation

--- Project/Server/server.js
+++ Project/Server/server.js
@@ -465,8 +465,7 @@ app.get('/venue/:venueId', async (req, res) => {
 app.post('/login', async (req, res) => {
   try {
     const { username, password } = req.body;
-
-    const user = await User.find({ username: username });
+    const user = await User.findOne({ username: username });
     if (!user) {
       return res.status(404).json({ error: 'Invalid username or password' });
     }
@@ -476,9 +475,9 @@ app.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid username or password' });
     }
 
-    const token = jwt.sign({ userId: user._id }, 'secretKey');
 
-    res.json({ token });
+    res.json({ user: user });
+
   } catch (error) {
     console.error('Error during login:', error);
     res.status(500).json({ error: 'An error occurred during login' });

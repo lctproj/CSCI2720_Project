3c88bc93fe2c42bd2f9f39111ee36bb98f8d61a4
iamtlcs <simoncheung2002@gmail.com>
2023-12-15 22:10:40 +0800

    CRUD

--- src/components/CreateEvent.jsx
+++ src/components/CreateEvent.jsx
@@ -25,12 +25,25 @@ function CreateEvent() {
         eventDates: [],
         venueId: "",
     });
-
     const handleInputChange = (e) => {
         const { name, value } = e.target;
         setEventData((prevState) => ({
             ...prevState,
             [name]: value,
+        }));
+    };
+
+    const handleDateChange = (key, date) => {
+        setEventData((prevState) => ({
+            ...prevState,
+            [key]: [...prevState[key], date],
+        }));
+    };
+
+    const handleClearDates = () => {
+        setEventData((prevState) => ({
+            ...prevState,
+            eventDates: [],
         }));
     };
 
@@ -40,7 +53,7 @@ function CreateEvent() {
             const token = Cookies.get("token");
 
             console.log(eventData);
-            eventData.eventId = 10
+            eventData.eventId = 10;
 
             const response = await fetch(
                 "http://localhost:8964/admin/create-event",
@@ -50,7 +63,7 @@ function CreateEvent() {
                         "Content-Type": "application/json",
                         Authorization: `Bearer ${token}`,
                     },
-                    body: JSON.stringify({ newEvent: eventData}),
+                    body: JSON.stringify({ newEvent: eventData }),
                 }
             );
 
@@ -66,6 +79,41 @@ function CreateEvent() {
         }
     };
 
+    const renderInputField = (key, value) => {
+        if (key.endsWith("Dates")) {
+            return (
+                <div>
+                    <input
+                        type="date"
+                        name={key}
+                        value={""}
+                        onChange={(e) => handleDateChange(key, e.target.value)}
+                        className="ml-2 px-2 py-1 border border-gray-300 rounded"
+                    />
+                    <ul>
+                        {eventData.eventDates.map((date, index) => (
+                            <li key={index}>{date}</li>
+                        ))}
+                    </ul>
+                    {eventData.eventDates.length > 0 && (
+                        <button onClick={handleClearDates}>Clear Dates</button>
+                    )}
+                </div>
+            );
+        }
+
+        return (
+            <input
+                type="text"
+                name={key}
+                value={value}
+                onChange={handleInputChange}
+                required={key.endsWith("Id")}
+                className="ml-2 px-2 py-1 border border-gray-300 rounded"
+            />
+        );
+    };
+
     return (
         <div className="p-4">
             <h2 className="text-2xl font-bold mb-4">Create Event</h2>
@@ -73,14 +121,7 @@ function CreateEvent() {
                 {Object.entries(eventData).map(([key, value]) => (
                     <div key={key} className="mb-2">
                         <label className="font-bold">{key}: </label>
-                        <input
-                            type="text"
-                            name={key}
-                            value={value}
-                            onChange={handleInputChange}
-                            required={key.endsWith("Id")}
-                            className="ml-2 px-2 py-1 border border-gray-300 rounded"
-                        />
+                        {renderInputField(key, value)}
                     </div>
                 ))}
                 <button
